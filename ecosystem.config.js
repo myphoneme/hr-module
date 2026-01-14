1fabcd0080d5fe8dd53c866e04be29f2c621144b
Aksa Kurian <aksakurian55@gmail.com>
2026-01-14 18:50:15 +0530

    congig file added

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: "hr-backend",
+      cwd: "/home/project/hr-module/server",
+      script: "index.js",            // change if needed
+      env_file: "/home/project/hr-module/.env",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "300M"
+    }
+  ]
+}
