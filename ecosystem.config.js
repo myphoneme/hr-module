84832ea50a765cef501eeb24a8a2f89eebd163dd
Aksa Kurian <aksakurian55@gmail.com>
2026-01-14 18:52:35 +0530

    config file edit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: "hr-backend",
       cwd: "/home/project/hr-module/server",
-      script: "index.js",            // change if needed
+      script: "index.ts",            // change if needed
       env_file: "/home/project/hr-module/.env",
       instances: 1,
       autorestart: true,
